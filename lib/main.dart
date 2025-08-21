2517920b63e1dcd16d705d01368dff78b82e48b2
halil <rubixb2github@gmail.com>
2025-08-21 11:03:18 +0300

    Notification sipariş tipi vs.. dil desteği

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'helpers/SessionManager.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
   print('📩 Arka planda mesaj: ${message.messageId}');
